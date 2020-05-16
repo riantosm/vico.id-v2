e36470037c924379526ccf48e70a898e17a8b87d
riantosm <rian.tosm@gmail.com>
2020-05-16 14:42:11 +0700

    add styles (fonts and colors)

---
+++ src/styles/colors/index.js
@@ -0,0 +1,12 @@
+export const colors = {
+  black: '#535353',
+  red: '#FC427B',
+  orange: '#F97F51',
+  green: '#2EBC34',
+  blue: '#3bbaf4',
+  blueDark: '#3c4379',
+  white: '#eef9fb',
+  gray: '#D1D1D1',
+  grayText: '#7E7E7E',
+  yellow: '#f9c635',
+};
---
+++ src/styles/fonts/index.js
@@ -0,0 +1,8 @@
+export const fonts = {
+  comfortaa_bold: 'comfortaa_bold',
+  comfortaa_regular: 'comfortaa_regular',
+  GoogleSans_Bold: 'GoogleSans_Bold',
+  GoogleSans_Reg: 'GoogleSans_Reg',
+  GothaProBol: 'GothaProBol',
+  GothaProReg: 'GothaProReg',
+};
